e6b104a1fa7e09d6fbd1e3c8ea6fa40c8704e7f0
Gekada <108587911+Gekada@users.noreply.github.com>
2025-05-05 16:00:17 +0300

    Added paginated requests

--- LB4/src/main/java/com/example/lb4/Common/Data/Dao/GroupDao.java
+++ LB4/src/main/java/com/example/lb4/Common/Data/Dao/GroupDao.java
@@ -21,6 +21,13 @@ public class GroupDao {
         return em.find(Group.class, id);
     }
 
+    public List<Group> getPage(int page, int size) {
+        return em.createQuery("SELECT g FROM Group g", Group.class)
+                .setFirstResult(page * size)
+                .setMaxResults(size)
+                .getResultList();
+    }
+
     public void update(Group group) {
         em.merge(group);
     }
--- LB4/src/main/java/com/example/lb4/Common/Data/Dao/StudentDao.java
+++ LB4/src/main/java/com/example/lb4/Common/Data/Dao/StudentDao.java
@@ -23,8 +23,11 @@ public class StudentDao {
         return em.find(Student.class, id);
     }
 
-    public List<Student> findAll() {
-        return em.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+    public List<Student> getPage(int page, int size) {
+        return em.createQuery("SELECT s FROM Student s", Student.class)
+                .setFirstResult(page * size)
+                .setMaxResults(size)
+                .getResultList();
     }
 
     public void update(Student student) {
